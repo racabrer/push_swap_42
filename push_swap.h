492629509e52a198e206a7d17e25e4ec22274a1b
Raquel Cabrero <raqcabre@student.42madrid.com>
2026-07-22 18:57:52 +0200

    me falta por gestionar mezcla de argumentos

--- push_swap.h
+++ push_swap.h
@@ -1,14 +1,13 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdbool.h>
 #include <limits.h>
 
 #define NONE        0
-#define SIMPLE		1
-#define MEDIUM		2
-#define COMPLEX		3
-#define ADAPTIVE	4
+#define SIMPLE      1
+#define MEDIUM      2
+#define COMPLEX     3
+#define ADAPTIVE    4
 
 typedef struct s_node
 {
@@ -26,16 +25,28 @@ typedef struct s_stack
 }	t_stack;
 
 
-//Errors
+/* Parser flags */
+int		flag_duplicates(char *flag, int strategy, int bench);
+int		set_strategy(char *flag, int *strategy);
+int		parse_flags(int argc, char **argv, int *strategy, int *bench);
+
+
+/* Parser numbers */
+int		parse_numbers(int argc, char **argv, int first_number, t_stack *a);
+int		is_number(char *str);
+long	ft_atol(const char *str);
+int		is_int_range(long value);
+int		has_duplicates(t_stack *a);
+
+
+/* Nodes */
+t_node	*create_node(int value);
+void	add_node(t_stack *stack, t_node *new_node);
+
+
+/* Errors / Stack */
 void	free_stack(t_stack *stack);
 void	exit_error(t_stack *a, t_stack *b);
 
-//Parsing
-int		parse_flags(int argc, char **argv, int *strategy, int *bench);
-int		parse_numbers(int argc, char **argv, int first_number, t_stack *a);
-int     is_number(char *str);
-long    ft_atol(const char *str);
-int     is_int_range(long value);
-int     has_duplicates(t_stack *a);
 
 #endif
