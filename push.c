ff9f60e869917aa5b0ef7212021ae59b79056f02
Raquel Cabrero <raqcabre@student.42madrid.com>
2026-08-03 18:30:54 +0200

    push

--- push.c
+++ push.c
@@ -2,40 +2,26 @@
 
 void	push(t_stack *dest, t_stack *src)
 {
-	t_node *moved_node;
+	t_node	*moved_node;
 
-	if (!src)
-		return;
+	if (!dest || !src || src->top)
+		return ;
 	moved_node = src->top;
-	
-
-
-
+	src->top = moved_node->next;
+	if (src->top)
+		src->top->prev = NULL;
+	else
+		src->bottom = NULL;
+	if (dest->top)
+		dest->top->prev = moved_node;
+	else
+		dest->bottom = moved_node;
+	moved_node->next = dest->top;
+	moved_node->prev = NULL;
+	dest->top = moved_node;
+	src->size--;
+	dest->size++;
 }
-
-/*
-mover el top de src al siguiente nodo
-
-    si src no está vacía
-        el nuevo top apunta a NULL por prev
-    si no
-        bottom de src pasa a NULL
-
-    si dest está vacía
-        el nodo movido será también el bottom
-    si no
-        el antiguo top de dest apunta hacia atrás al nodo movido
-
-    el nodo movido apunta al antiguo top de dest
-
-    el nodo movido no tiene prev
-
-    el top de dest pasa a ser el nodo movido
-
-    actualizar los tamaños
-
-fin
-*/
 
 void	pa(t_stack *a, t_stack *b)
 {
