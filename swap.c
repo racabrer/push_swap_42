32ccb27c51e44a04af9c34fc3a457f2fae6e68bb
Raquel Cabrero <raqcabre@student.42madrid.com>
2026-08-03 18:41:02 +0200

    all movements finished

--- swap.c
+++ swap.c
@@ -20,16 +20,21 @@ void	swap_node(t_stack *stack)
 	stack->top = second;
 }
 
-void	sa(t_stack *a, bool checker)
+void	sa(t_stack *a)
 {
 	swap_node(a);
-	if (!checker)
-		write(1, "sa\n", 3);
+	write(1, "sa\n", 3);
 }
 
-void	sb(t_stack *b, bool checker)
+void	sb(t_stack *b)
 {
 	swap_node(b);
-	if (!checker)
-		write(1, "sb\n", 3);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_stack *a, t_stack *b)
+{
+	swap_node(a);
+	swap_node(b);
+	write(1, "ss\n", 3);
 }
